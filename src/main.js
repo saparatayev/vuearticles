cc9e67e84446cf295a98c861d5751de507300603
saparatayev <sopa.97@mail.ru>
2021-06-28 09:12:36 +0500

    fix: failed to resolve component spinner

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Navbar from './components/Navbar.vue'
+import Spinner from './components/Spinner.vue'
 import i18n from './i18n'
 import store from './store/store.js'
 import Axios from 'axios'
@@ -9,6 +10,7 @@ import router from './routes';
 const app = createApp(App).use(i18n).use(store).use(router)
 
 app.component('navbar', Navbar);
+app.component('spinner', Spinner);
 
 router.beforeEach((to,from,next) => {
     let language = to.params.lang;
